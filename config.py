e1d8800c33050250e06f8d93a53b718ee1604e74
Hajimider <3247731464@qq.com>
2026-06-17 19:06:24 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-DB_CONFIG = {
-    'host': 'localhost',
-    'port': 3306,
-    'user': 'root',
-    'password': '123456',  # 请修改为您的密码
-    'database': 'sales_db',
-    'charset': 'utf8mb4'
-}
