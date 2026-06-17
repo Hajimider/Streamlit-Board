0878bc91a2b0f27fd1e34430c71273be0226efe0
Hajimider <3247731464@qq.com>
2026-06-17 17:54:05 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'password': '123456',  # 请修改为您的密码
+    'database': 'sales_db',
+    'charset': 'utf8mb4'
+}
